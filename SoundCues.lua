455a36d6020d4491848c9970a9bd4a16e07dac05
Rymorc <bart.buyens5@gmail.com>
2025-05-25 15:30:42 +0200

    menu work in progress

--- SoundCues.lua
+++ SoundCues.lua
@@ -1,34 +1,30 @@
-SoundCues = {}
+SoundCues = SoundCues or {}
 
 SoundCues.name = "SoundCues"
+SoundCues.version = "1.0.1"
+SoundCues.author = "@Rymorc"
+SoundCues.Settings = {}
 
-function SoundCues.Initialize()
-    SoundCues.activeBuffs = {}
-
-    EVENT_MANAGER:RegisterForEvent(SoundCues.name, EVENT_EFFECT_CHANGED, SoundCues.onEffectChanged)
-end
-
-function SoundCues.OnAddOnLoaded(event, addonName)
-    if addonName == SoundCues.name then
-        SoundCues.Initialize()
-        EVENT_MANAGER:UnregisterForEvent(SoundCues.name, EVENT_ADD_ON_LOADED)
-        EVENT_MANAGER:AddFilterForEvent(SoundCues.name, EVENT_EFFECT_CHANGED, REGISTER_FILTER_UNIT_TAG, "player")
-    end
+function SoundCues.PlaySound(sound, volume)
+	if volume < 1 or volume > 20 then volume = 1 end
+	for i = 1, volume do -- looping a sound increases its volume
+		PlaySound(SOUNDS[sound])
+	end
 end
 
 function SoundCues.onEffectChanged(eventCode, changeType, effectSlot, effectName, unitTag, beginTime, endTime, stackCount, iconName, buffType, effectType, abilityType, statusEffectType, unitName, unitId, abilityId, sourceUnitType)
-    for effectId, buffData in pairs(SoundCuesData.buffs) do
-        if (effectId == abilityId) then
+    for effectId, buffData in pairs(SoundCues.Settings.trackedEffects) do
+        if effectId == abilityId and SoundCues.Settings.trackedEffects[effectId].active then
             if (changeType == EFFECT_RESULT_FADED) then
                 SoundCues.activeBuffs[effectId] = nil
                 if buffData.timeBeforeEnd == 0.0 then
-                    PlaySound(buffData.sound)
+                    SoundCues.PlaySound(buffData.sound, buffData.volume)
                 end
             elseif (changeType == EFFECT_RESULT_GAINED) then
                 if buffData.timeBeforeEnd ~= 0.0 then
                     SoundCues.activeBuffs[effectId] = {
                         endTime = endTime,
-                        alerted = false,
+                        count = 0,
                     }
                     EVENT_MANAGER:RegisterForUpdate("SoundCuesRun", 100, SoundCues.run)
                 end
@@ -39,17 +35,32 @@ end
 
 function SoundCues.run()
     local now = GetGameTimeSeconds()
-    local tableIsEmpty = true
+    local noActiveBuffs = true
     for effectId, activeBuffData in pairs(SoundCues.activeBuffs) do
-        tableIsEmpty = false
-        if activeBuffData.endTime - SoundCuesData.buffs[effectId].timeBeforeEnd <= now and not activeBuffData.alerted then
-            PlaySound(SoundCuesData.buffs[effectId].sound)
-            activeBuffData.alerted = true
+        noActiveBuffs = false
+        local buffData = SoundCues.Settings.trackedEffects[effectId]
+        if activeBuffData.endTime - buffData.timeBeforeEnd <= now and activeBuffData.count < buffData.sound_amount then
+            SoundCues.PlaySound(buffData.sound, buffData.volume)
+            activeBuffData.count = activeBuffData.count + 1
         end
     end
-    if tableIsEmpty then
+    if noActiveBuffs then
         EVENT_MANAGER:UnregisterForUpdate("SoundCuesRun")
     end
+end
+
+function SoundCues.Initialize()
+    SoundCues.activeBuffs = {}
+    SoundCues.Settings = ZO_SavedVars:New("SoundCuesSavedVariables", 2, nil, SoundCuesData.defaults) -- TODO reset back to 1
+    SoundCues.setUpMenu()
+    EVENT_MANAGER:RegisterForEvent(SoundCues.name, EVENT_EFFECT_CHANGED, SoundCues.onEffectChanged)
+end
+
+function SoundCues.OnAddOnLoaded(event, addonName)
+    if addonName ~= SoundCues.name then return end
+    EVENT_MANAGER:UnregisterForEvent(SoundCues.name, EVENT_ADD_ON_LOADED)
+    SoundCues.Initialize()
+    EVENT_MANAGER:AddFilterForEvent(SoundCues.name, EVENT_EFFECT_CHANGED, REGISTER_FILTER_UNIT_TAG, "player") -- Only track player events
 end
 
 EVENT_MANAGER:RegisterForEvent(SoundCues.name, EVENT_ADD_ON_LOADED, SoundCues.OnAddOnLoaded)
---
+++ SoundCuesMenu.lua
@@ -0,0 +1,117 @@
+SoundCues = SoundCues or {}
+
+local menuCurrentEffectId = nil
+
+local function getTrackedEffectLists(tbl)
+    local EffectIdList = {}
+    local EffectNameList = {}
+    for EffectId, effectSettings in pairs(tbl) do
+        table.insert(EffectIdList, EffectId)
+        table.insert(EffectNameList, effectSettings.name)
+    end
+    return EffectIdList, EffectNameList
+end
+
+local function noMenuCurrentEffectId()
+    if menuCurrentEffectId == nil then
+        return true
+    else
+        return false
+    end
+end
+
+local function getEffectValue(attribute)
+    if menuCurrentEffectId == nil then return nil end
+    return SoundCues.Settings.trackedEffects[menuCurrentEffectId][attribute]
+end
+
+local function setEffectValue(attribute, value)
+    if menuCurrentEffectId == nil then return end
+    SoundCues.Settings.trackedEffects[menuCurrentEffectId][attribute] = value
+end
+
+local function testSound(value)
+    SoundCues.PlaySound(getEffectValue("sound"), getEffectValue("volume"))
+end
+
+function SoundCues.setUpMenu()
+    local LAM2 = LibAddonMenu2
+    local EffectIdList, EffectNameList = getTrackedEffectLists(SoundCues.Settings.trackedEffects)
+    -- df("EffectNameList: %s, %s, %s", unpack(EffectNameList))
+
+    local panelData = {
+        type = "panel",
+        name = "SoundCues",
+        registerForRefresh = true,
+        slashCommand = "/SoundCues",
+    }
+    local optionsData = {
+        [1] = {
+            type = "dropdown",
+            name = "Effect",
+            tooltip = "Tracked effect",
+            choices = EffectNameList,
+            choicesValues = EffectIdList,
+            getFunc = function() return menuCurrentEffectId end,
+            setFunc = function(value) menuCurrentEffectId = value end,
+            scrollable = true,
+            sort = "name-up",
+            width = "half",
+        },
+        [2] = {
+            type = "divider",
+            width = "full",
+        },
+        [3] = {
+            type = "checkbox",
+            name = "Active",
+            getFunc = function() return getEffectValue("active") end,
+            setFunc = function(value) setEffectValue("active", value) end,
+            tooltip = "Effect Tracking is active",
+            width = "full",
+            disabled = noMenuCurrentEffectId,
+        },
+        [4] = {
+            type = "header",
+            name = "Sound Effect",
+            width = "full",
+        },
+        [5] = {
+            type = "dropdown",
+            name = "Sound Effect",
+            tooltip = "Sound Effect to play",
+            choices = SoundCuesData.soundList,
+            scrollable = true,
+            getFunc = function() return getEffectValue("sound") end,
+            setFunc = function(value) setEffectValue("sound", value) end,
+            width = "half",
+            disabled = noMenuCurrentEffectId,
+        },
+        [6] = {
+            type = "slider",
+            name = "Volume",
+            tooltip = "Volume of the Sound Effect",
+            min = 1,
+            max = 20,
+            getFunc = function() return getEffectValue("volume") end,
+            setFunc = function(value) setEffectValue("volume", value) end,
+            width = "half",
+            disabled = noMenuCurrentEffectId,
+        },
+        [7] = {
+                type = "button",
+                name = "Test",
+                tooltip = "Test Sound Effect",
+                func = testSound,
+                width = "full",
+                disabled = noMenuCurrentEffectId
+        },
+        [8] = {
+            type = "divider",
+            width = "full",
+        },
+    }
+
+    LAM2:RegisterAddonPanel("SoundCuesOptions", panelData)
+    LAM2:RegisterOptionControls("SoundCuesOptions", optionsData)
+end
